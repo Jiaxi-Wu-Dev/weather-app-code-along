d1151509938afcbcb5eb6ba40b6f686c6364618b
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-09 14:13:13 -0400

    api key

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React from 'react';
-
 import './App.css';
+import axios from 'axios';
+import Weather from './components/Weather';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    </div>
-  );
+    this.state = {
+      weather: null,
+    }
+  }
+
+  componentDidMount() {
+    this.getWeatherData();
+  }
+
+  // async getWeatherData() {
+  //   try {
+  //     const response = await axios.get('https://api.openweathermap.org/data/2.5/forecast?q=london&appid=e4447104f032368a2a14af1ccf6fd22e&units=imperial');
+
+  //     this.setState({ weather: response.data });
+  //   } catch(error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  getWeatherData() {
+    axios.get('https://api.openweathermap.org/data/2.5/forecast?q=london&appid=e4447104f032368a2a14af1ccf6fd22e&units=imperial')
+      .then(response => {
+        console.log(response.data);
+        this.setState({ weather: response.data });
+      }).catch(error => console.error(error));
+  }
+
+  nullcheck() {
+    if (this.state.weather === null) {
+      return <h2>no information</h2>
+    } else {
+      this.state.weather.list.map(listItem => <Weather />);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            this.nullcheck()
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
